4de8d7fa5e596578b703b1bdd2321072b40f3598
Sparsh Verma <sparsh.red@gmail.com>
2022-10-24 19:40:10 +0530

    Added solution - LeetHub

---
+++ Earthquake and the Paint Shop - GFG/earthquake-and-the-paint-shop.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            String s[] = new String[N];
+            for (int i = 0; i < N; i++) s[i] = read.readLine();
+            Solution ob = new Solution();
+            alphanumeric ans[] = ob.sortedStrings(N, s);
+            for (int i = 0; i < ans.length; i++)
+                System.out.println(ans[i].name + " " + ans[i].count);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class alphanumeric {
+    public String name;
+    public int count;
+    alphanumeric(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+};
+class Solution {
+    alphanumeric[] sortedStrings(int N, String A[]) {
+        
+    //     LinkedHashMap<String,Integer> hm = new LinkedHashMap<>();
+
+    //   Arrays.sort(A);
+
+    //     for(int i=0;i<A.length;i++){
+
+    //         hm.put(A[i], hm.getOrDefault(A[i],0)+1);
+
+    //     }
+
+    //     alphanumeric al[]=new alphanumeric[hm.size()];
+
+    //     int i1=0;
+
+    //     for(Map.Entry<String,Integer>i:hm.entrySet()){
+
+    //         al[i1]=new alphanumeric(i.getKey(),i.getValue());
+
+    //         i1++;
+
+    //     }
+
+    //   // Arrays.sort(al);
+
+    //     return al;
+    
+        TreeMap<String, Integer> map = new TreeMap<>();
+        
+        for(String s: A) {
+            map.put(s, map.getOrDefault(s, 0)+1);
+        }
+        
+        alphanumeric al[]=new alphanumeric[map.size()];
+        int i=0;
+        
+        for(String s: map.keySet()){
+            al[i++] = new alphanumeric(s, map.get(s));
+        }
+        
+        return al;
+    }
+};
