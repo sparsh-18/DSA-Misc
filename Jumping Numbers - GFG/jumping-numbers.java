722551ec372ab75bef08d81b8d52fee2cbbe4c87
Sparsh Verma <sparsh.red@gmail.com>
2022-11-15 22:52:55 +0530

    Added solution - LeetHub

--- Jumping Numbers - GFG/jumping-numbers.java
+++ Jumping Numbers - GFG/jumping-numbers.java
@@ -26,49 +26,37 @@ class Solution {
     static long jumpingNums(long X) {
         // code here
         Queue<Long> q = new LinkedList<>();
-
-        long max = 0;
-
-        for(long i = 0; i < 10; i++){
-
+        
+        for(long i=0; i<=9; i++)
             q.add(i);
-
-        }
-
-        while(!q.isEmpty()){
-
-            long num = q.remove();
-
-            if(num > X){
-
-                continue;
-
-            }
-
             
-
-            long lastDigit = num % 10;
-
-            if(lastDigit != 0){
-
-                long first = num * 10 + (lastDigit - 1);
-
-                q.add(first);
-
+        long ans = 0l;
+        
+        while(!q.isEmpty()) {
+            long n = q.remove();
+            
+            if(n > X || n == 0)
+                continue;
+            
+            long last_dig = n % 10;
+            
+            long n01 = n*10 + last_dig - 1;
+            long n02 = n*10 + last_dig + 1;
+            
+            
+            if(n01 <= X && last_dig != 0) {
+                q.add(n01);
+                ans = Math.max(ans, n01);
             }
-
-            if(lastDigit != 9){
-
-                long sec = num * 10 + (lastDigit + 1);
-
-                q.add(sec);
-
+                
+            
+            if(n02 <= X && last_dig != 9) {
+                q.add(n02);
+                ans = Math.max(ans, n02);
             }
-
-            max = Math.max(num, max);
-
+            
         }
-
-        return max;
+        
+        return ans;
     }
 };
